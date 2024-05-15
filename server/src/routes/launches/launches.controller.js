35291419d64e1e0afb90f17a7d7f52b1caad60a6
SudamAtCashflo <sudam.swain@cashflo.io>
2024-05-15 09:36:28 +0530

    added correct response status for planet not found

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -30,7 +30,7 @@ async function httpAddNewLaunch(req, res){
       await scheduleNewLaunch(launch);
       return res.status(201).json(launch);
     } catch (error){
-      res.status(500).json({ error: error.message });
+      res.status(400).json({ error: error.message });
     }
 
 }
